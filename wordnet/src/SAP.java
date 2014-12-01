895073e95d720ae809b88eccf4f5463a64561f00
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-01 18:54:56 +0300

    sap

--- wordnet/src/SAP.java
+++ wordnet/src/SAP.java
@@ -25,8 +25,16 @@ public class SAP {
       return -1;
    }
 
-   // do unit testing of this class
    public static void main(String[] args) {
-
+      In in = new In(args[0]);
+      Digraph G = new Digraph(in);
+      SAP sap = new SAP(G);
+      while (!StdIn.isEmpty()) {
+         int v = StdIn.readInt();
+         int w = StdIn.readInt();
+         int length   = sap.length(v, w);
+         int ancestor = sap.ancestor(v, w);
+         StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+      }
    }
 }
