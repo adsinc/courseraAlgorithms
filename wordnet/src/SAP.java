d4d0ac1b20bbfb3ee6bc331666346b4af0f0c766
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-02 19:53:35 +0300

    worknet

--- wordnet/src/SAP.java
+++ wordnet/src/SAP.java
@@ -1,88 +1,88 @@
 public class SAP {
 
-   private Digraph G;
+	private Digraph G;
 
-   // constructor takes a digraph (not necessarily a DAG)
-   public SAP(Digraph G) {
-      this.G = G;
-   }
+	// constructor takes a digraph (not necessarily a DAG)
+	public SAP(Digraph G) {
+		this.G = G;
+	}
 
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-      if(sv.hasPathTo(w)) return sv.distTo(v);
-      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
-      int len = -1;
-      for(int i = 0; i < G.V(); i++) {
-         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
-            int l = sv.distTo(i) + sw.distTo(i);
-            if(l < len || len == -1) len = l;
-         }
-      }
-      return len;
-   }
+	// length of shortest ancestral path between v and w; -1 if no such path
+	public int length(int v, int w) {
+		BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+		if (sv.hasPathTo(w)) return sv.distTo(v);
+		BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+		int len = -1;
+		for (int i = 0; i < G.V(); i++) {
+			if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
+				int l = sv.distTo(i) + sw.distTo(i);
+				if (l < len || len == -1) len = l;
+			}
+		}
+		return len;
+	}
 
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w) {
-      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-      if(sv.hasPathTo(w)) return sv.distTo(v);
-      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
-      int len = -1;
-      int a = -1;
-      for(int i = 0; i < G.V(); i++) {
-         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
-            int l = sv.distTo(i) + sw.distTo(i);
-            if(l < len || len == -1) {
-               len = l;
-               a = i;
-            }
-         }
-      }
-      return a;
-   }
+	// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+	public int ancestor(int v, int w) {
+		BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+		if (sv.hasPathTo(w)) return sv.distTo(v);
+		BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+		int len = -1;
+		int a = -1;
+		for (int i = 0; i < G.V(); i++) {
+			if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
+				int l = sv.distTo(i) + sw.distTo(i);
+				if (l < len || len == -1) {
+					len = l;
+					a = i;
+				}
+			}
+		}
+		return a;
+	}
 
-   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-   public int length(Iterable<Integer> v, Iterable<Integer> w) {
-      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
-      int len = -1;
-      for(int i = 0; i < G.V(); i++) {
-         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
-            int l = sv.distTo(i) + sw.distTo(i);
-            if(l < len || len == -1) len = l;
-         }
-      }
-      return len;
-   }
+	// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+	public int length(Iterable<Integer> v, Iterable<Integer> w) {
+		BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+		BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+		int len = -1;
+		for (int i = 0; i < G.V(); i++) {
+			if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
+				int l = sv.distTo(i) + sw.distTo(i);
+				if (l < len || len == -1) len = l;
+			}
+		}
+		return len;
+	}
 
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
-      int len = -1;
-      int a = -1;
-      for(int i = 0; i < G.V(); i++) {
-         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
-            int l = sv.distTo(i) + sw.distTo(i);
-            if(l < len || len == -1) {
-               len = l;
-               a = i;
-            }
-         }
-      }
-      return a;
-   }
+	// a common ancestor that participates in shortest ancestral path; -1 if no such path
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+		BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+		BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+		int len = -1;
+		int a = -1;
+		for (int i = 0; i < G.V(); i++) {
+			if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
+				int l = sv.distTo(i) + sw.distTo(i);
+				if (l < len || len == -1) {
+					len = l;
+					a = i;
+				}
+			}
+		}
+		return a;
+	}
 
-   public static void main(String[] args) {
-      In in = new In(args[0]);
-      Digraph G = new Digraph(in);
-      SAP sap = new SAP(G);
-      while (!StdIn.isEmpty()) {
-         int v = StdIn.readInt();
-         int w = StdIn.readInt();
-         int length   = sap.length(v, w);
-         int ancestor = sap.ancestor(v, w);
-         StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-      }
-   }
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		Digraph G = new Digraph(in);
+		SAP sap = new SAP(G);
+		while (!StdIn.isEmpty()) {
+			int v = StdIn.readInt();
+			int w = StdIn.readInt();
+			int length = sap.length(v, w);
+			int ancestor = sap.ancestor(v, w);
+			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+		}
+	}
 }
--- wordnet/src/WordNet.java
+++ wordnet/src/WordNet.java
@@ -2,90 +2,84 @@ import java.util.*;
 
 public class WordNet {
 
-   private Map<Integer, Set<String>> synsetsMap = new HashMap<>();
-   private Map<Integer, String> glossMap = new HashMap<>();
-   private Digraph hg;
-   private SAP sap;
+	private Map<String, Integer> syns = new HashMap<>();
+	private Map<Integer, String> glossMap = new HashMap<>();
+	private Digraph hg;
+	private SAP sap;
 
-   // constructor takes the name of the two input files
-   public WordNet(String synsets, String hypernyms) {
-      readSynsets(synsets);
-      readHypernyms(hypernyms);
-      sap = new SAP(hg);
-   }
+	// constructor takes the name of the two input files
+	public WordNet(String synsets, String hypernyms) {
+		readSynsets(synsets);
+		readHypernyms(hypernyms);
+		sap = new SAP(hg);
+	}
 
-   private void readSynsets(String synsets) {
-      // 36,AND_circuit AND_gate,a circuit in a computer that fires only when all of its inputs fire
-      In ssIn = new In(synsets);
-      String[] lines = ssIn.readAllStrings();
-      for (String line: lines) {
-         String[] lex = line.split(",");
-         try {
-            Integer id = Integer.parseInt(lex[0]);
-            Set<String> sset = new HashSet<>();
-            Collections.addAll(sset, lex[1].split(" "));
-            synsetsMap.put(id, sset);
-            glossMap.put(id, lex[2]);
-         } catch (Exception e) {
-            throw new IllegalArgumentException();
-         }
-      }
-   }
+	private void readSynsets(String synsets) {
+		// 36,AND_circuit AND_gate,a circuit in a computer that fires only when all of its inputs fire
+		In ssIn = new In(synsets);
+		String[] lines = ssIn.readAllStrings();
+		for (String line : lines) {
+			String[] lex = line.split(",");
+			try {
+				Integer id = Integer.parseInt(lex[0]);
+				for (String l : lex[1].split(" "))
+					syns.put(l, id);
+				glossMap.put(id, lex[2]);
+			} catch (Exception e) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
 
-   private void readHypernyms(String hypernyms) {
-      hg = new Digraph(glossMap.size());
-      //164,21012,56099
-      In hnIn = new In(hypernyms);
-      String[] lines = hnIn.readAllStrings();
-      for(String line: lines) {
-         String[] lex = line.split(",");
-         try {
-            Integer id = Integer.parseInt(lex[0]);
-            for (int i = 1; i < lex.length; i++)
-               hg.addEdge(id, Integer.parseInt(lex[i]));
-         } catch (Exception e) {
-            throw new IllegalArgumentException();
-         }
-      }
-   }
+	private void readHypernyms(String hypernyms) {
+		hg = new Digraph(glossMap.size());
+		//164,21012,56099
+		In hnIn = new In(hypernyms);
+		String[] lines = hnIn.readAllStrings();
+		for (String line : lines) {
+			String[] lex = line.split(",");
+			try {
+				Integer id = Integer.parseInt(lex[0]);
+				for (int i = 1; i < lex.length; i++)
+					hg.addEdge(id, Integer.parseInt(lex[i]));
+			} catch (Exception e) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
 
-   private Set<String> nounsSet() {
-      Set<String> nouns = new HashSet<>();
-      for(Set<String> s : synsetsMap.values())
-         nouns.addAll(s);
-      return nouns;
-   }
+	// returns all WordNet nouns
+	public Iterable<String> nouns() {
+		return syns.keySet();
+	}
 
-   // returns all WordNet nouns
-   public Iterable<String> nouns() {
-       return nounsSet();
-   }
+	// is the word a WordNet noun?
+	public boolean isNoun(String word) {
+		return syns.keySet().contains(word);
+	}
 
-   // is the word a WordNet noun?
-   public boolean isNoun(String word) {
-      return nounsSet().contains(word);
-   }
+	// distance between nounA and nounB (defined below)
+	public int distance(String nounA, String nounB) {
+		return sap.length(syns.get(nounA), syns.get(nounB));
+	}
 
-   // distance between nounA and nounB (defined below)
-   public int distance(String nounA, String nounB) {
+	// a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+	// in a shortest ancestral path (defined below)
+	public String sap(String nounA, String nounB) {
+		int id = sap.ancestor(syns.get(nounA), syns.get(nounB));
+		for (Map.Entry<String, Integer> e : syns.entrySet()) {
+			if (id == e.getValue()) return e.getKey();
+		}
+		return null;
+	}
 
-//      return sap.length();
-   }
-
-   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-   // in a shortest ancestral path (defined below)
-   public String sap(String nounA, String nounB) {
-      return null;
-   }
-
-   // do unit testing of this class
-   public static void main(String[] args) {
-      String synset = "wordnet/examples/synsets11.txt";
-      String hypernym = "wordnet/examples/hypernymsManyPathsOneAncestor.txt";
-      WordNet net = new WordNet(synset, hypernym);
-      net.nounsSet();
-      System.out.println(net.isNoun("a"));
-      System.out.println(net.isNoun("b"));
-      System.out.println(net.isNoun("z"));
-   }
+	// do unit testing of this class
+	public static void main(String[] args) {
+		String synset = "wordnet/examples/synsets11.txt";
+		String hypernym = "wordnet/examples/hypernymsManyPathsOneAncestor.txt";
+		WordNet net = new WordNet(synset, hypernym);
+		System.out.println(net.isNoun("a"));
+		System.out.println(net.isNoun("b"));
+		System.out.println(net.isNoun("z"));
+	}
 }
