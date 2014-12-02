43c73394c1e029b1bd050234edaaac2925abd483
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-02 18:12:29 +0300

    ancestor

--- wordnet/src/SAP.java
+++ wordnet/src/SAP.java
@@ -43,12 +43,34 @@ public class SAP {
 
    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-      return -1;
+      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+      int len = -1;
+      for(int i = 0; i < G.V(); i++) {
+         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
+            int l = sv.distTo(i) + sw.distTo(i);
+            if(l < len || len == -1) len = l;
+         }
+      }
+      return len;
    }
 
    // a common ancestor that participates in shortest ancestral path; -1 if no such path
    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-      return -1;
+      BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
+      BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+      int len = -1;
+      int a = -1;
+      for(int i = 0; i < G.V(); i++) {
+         if(sv.hasPathTo(i) && sw.hasPathTo(i)) {
+            int l = sv.distTo(i) + sw.distTo(i);
+            if(l < len || len == -1) {
+               len = l;
+               a = i;
+            }
+         }
+      }
+      return a;
    }
 
    public static void main(String[] args) {
