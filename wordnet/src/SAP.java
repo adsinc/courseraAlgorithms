7e4148355d29fdd9725443e35afc064e6ea0f972
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-03 11:56:42 +0300

    caching

--- wordnet/src/SAP.java
+++ wordnet/src/SAP.java
@@ -1,17 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SAP {
 
     private Digraph G;
 
+    private Map<Integer, BreadthFirstDirectedPaths> cache = new HashMap<>();
+
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-        this.G = G;
+        for(int v = 0; v < G.V(); v++)
+            cache.put(v, new BreadthFirstDirectedPaths(G, v));
     }
 
     // length of shortest ancestral path between v and w;
     // -1 if no such path
     public int length(int v, int w) {
-        BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-        BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+        BreadthFirstDirectedPaths sv = cache.get(v);
+        BreadthFirstDirectedPaths sw = cache.get(w);
         int len = -1;
         for (int i = 0; i < G.V(); i++) {
             if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
@@ -25,8 +31,8 @@ public class SAP {
     // a common ancestor of v and w that participates in a
     // shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-        BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+        BreadthFirstDirectedPaths sv = cache.get(v);
+        BreadthFirstDirectedPaths sw = cache.get(w);
         int len = -1;
         int a = -1;
         for (int i = 0; i < G.V(); i++) {
@@ -44,8 +50,8 @@ public class SAP {
     // length of shortest ancestral path between any vertex in v and
     // any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-        BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+        BreadthFirstDirectedPaths sv = cache.get(v);
+        BreadthFirstDirectedPaths sw = cache.get(w);
         int len = -1;
         for (int i = 0; i < G.V(); i++) {
             if (sv.hasPathTo(i) && sw.hasPathTo(i)) {
@@ -59,8 +65,8 @@ public class SAP {
     // a common ancestor that participates in shortest ancestral path;
     // -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        BreadthFirstDirectedPaths sv = new BreadthFirstDirectedPaths(G, v);
-        BreadthFirstDirectedPaths sw = new BreadthFirstDirectedPaths(G, w);
+        BreadthFirstDirectedPaths sv = cache.get(v);
+        BreadthFirstDirectedPaths sw = cache.get(w);
         int len = -1;
         int a = -1;
         for (int i = 0; i < G.V(); i++) {
