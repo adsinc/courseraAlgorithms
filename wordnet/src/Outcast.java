9c0f8307c56dae5b5bd99751cdd263ba30d50287
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-04 18:02:13 +0300

    outcast

--- wordnet/src/Outcast.java
+++ wordnet/src/Outcast.java
@@ -1,12 +1,40 @@
 public class Outcast {
+
+    private WordNet wordnet;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        this.wordnet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-        return null;
+        /**
+         * Outcast detection. Given a list of wordnet nouns A1, A2, ...,
+         * An, which noun is the least related to the others?
+         * To identify an outcast, compute the sum of the distances between
+         * each noun and every other one:
+         * di = dist(Ai, A1) + dist(Ai, A2) +   ...   + dist(Ai, An)
+         * and return a noun At for which dt is maximum.
+         */
+        int max = -1;
+        String maxWord = null;
+        for (String noun : nouns) {
+            int d = dist(nouns, noun);
+            if (d > max || max == -1) {
+                max = d;
+                maxWord = noun;
+            }
+        }
+        return maxWord;
+    }
+
+    private int dist(String[] nouns, String word) {
+        int d = 0;
+        for (String noun : nouns) {
+            d += wordnet.distance(word, noun);
+        }
+        return d;
     }
 
     public static void main(String[] args) {
