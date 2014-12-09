0365ea78170be4dd65f0fffa10a536ba8a74f5d0
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-09 18:51:09 +0300

    checks

--- seamCarving/src/SeamCarver.java
+++ seamCarving/src/SeamCarver.java
@@ -1,84 +1,96 @@
 public class SeamCarver {
-	/**
-	 * create a seam carver object based on the given picture
-	 *
-	 * @param picture
-	 */
-	public SeamCarver(Picture picture) {
 
-	}
+    private Picture picture;
 
-	/**
-	 * current picture
-	 *
-	 * @return
-	 */
-	public Picture picture() {
-		return null;
-	}
+    /**
+     * create a seam carver object based on the given picture
+     *
+     * @param picture
+     */
+    public SeamCarver(Picture picture) {
+        this.picture = new Picture(picture);
+    }
 
-	/**
-	 * width of current picture
-	 *
-	 * @return
-	 */
-	public int width() {
-		return -1;
-	}
+    /**
+     * current picture
+     *
+     * @return
+     */
+    public Picture picture() {
+        return null;
+    }
 
-	/**
-	 * height of current picture
-	 *
-	 * @return
-	 */
-	public int height() {
-		return -1;
-	}
+    /**
+     * width of current picture
+     *
+     * @return
+     */
+    public int width() {
+        return picture.width();
+    }
 
-	/**
-	 * energy of pixel at column x and row y
-	 *
-	 * @param x
-	 * @param y
-	 * @return
-	 */
-	public double energy(int x, int y) {
-		return -1;
-	}
+    /**
+     * height of current picture
+     *
+     * @return
+     */
+    public int height() {
+        return picture.height();
+    }
 
-	/**
-	 * sequence of indices for horizontal seam
-	 *
-	 * @return
-	 */
-	public int[] findHorizontalSeam() {
-		return null;
-	}
+    /**
+     * energy of pixel at column x and row y
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public double energy(int x, int y) {
+        if(x < 0 || y < 0 
+                || x > width() - 1 || y > height() - 1)
+            throw new IndexOutOfBoundsException();
+        return -1;
+    }
 
-	/**
-	 * sequence of indices for vertical seam
-	 *
-	 * @return
-	 */
-	public int[] findVerticalSeam() {
-		return null;
-	}
+    /**
+     * sequence of indices for horizontal seam
+     *
+     * @return
+     */
+    public int[] findHorizontalSeam() {
+        return null;
+    }
 
-	/**
-	 * remove horizontal seam from current picture
-	 *
-	 * @param seam
-	 */
-	public void removeHorizontalSeam(int[] seam) {
+    /**
+     * sequence of indices for vertical seam
+     *
+     * @return
+     */
+    public int[] findVerticalSeam() {
+        return null;
+    }
 
-	}
+    /**
+     * remove horizontal seam from current picture
+     *
+     * @param seam
+     */
+    public void removeHorizontalSeam(int[] seam) {
+		checkSeam(seam, width());
+    }
 
-	/**
-	 * remove vertical seam from current picture
-	 *
-	 * @param seam
-	 */
-	public void removeVerticalSeam(int[] seam) {
+    /**
+     * remove vertical seam from current picture
+     *
+     * @param seam
+     */
+    public void removeVerticalSeam(int[] seam) {
+		checkSeam(seam, height());
+    }
 
+	private void checkSeam(int[] seam, int required) {
+		if(seam == null) throw new NullPointerException();
+		if(seam.length != required)
+			throw new IllegalArgumentException("seam.length must be " + required);
 	}
 }
