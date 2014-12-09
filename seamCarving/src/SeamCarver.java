5b37b8401f9cb49ac6abf7d534d48f3444f8d80b
Aleksei Dolgii <dolgiy@spb.ant-inform.ru>
2014-12-09 18:40:20 +0300

    init

---
+++ seamCarving/src/SeamCarver.java
@@ -0,0 +1,84 @@
+public class SeamCarver {
+	/**
+	 * create a seam carver object based on the given picture
+	 *
+	 * @param picture
+	 */
+	public SeamCarver(Picture picture) {
+
+	}
+
+	/**
+	 * current picture
+	 *
+	 * @return
+	 */
+	public Picture picture() {
+		return null;
+	}
+
+	/**
+	 * width of current picture
+	 *
+	 * @return
+	 */
+	public int width() {
+		return -1;
+	}
+
+	/**
+	 * height of current picture
+	 *
+	 * @return
+	 */
+	public int height() {
+		return -1;
+	}
+
+	/**
+	 * energy of pixel at column x and row y
+	 *
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public double energy(int x, int y) {
+		return -1;
+	}
+
+	/**
+	 * sequence of indices for horizontal seam
+	 *
+	 * @return
+	 */
+	public int[] findHorizontalSeam() {
+		return null;
+	}
+
+	/**
+	 * sequence of indices for vertical seam
+	 *
+	 * @return
+	 */
+	public int[] findVerticalSeam() {
+		return null;
+	}
+
+	/**
+	 * remove horizontal seam from current picture
+	 *
+	 * @param seam
+	 */
+	public void removeHorizontalSeam(int[] seam) {
+
+	}
+
+	/**
+	 * remove vertical seam from current picture
+	 *
+	 * @param seam
+	 */
+	public void removeVerticalSeam(int[] seam) {
+
+	}
+}
